9872a7485d4db1f159a19c6146309e9778d0ba95
maxbarsukov <maximbarsukov@bk.ru>
2025-01-21 20:56:47 +0300

    feat(frontend): :sparkles: add navbar

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -3,4 +3,10 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: 'Sleepy',
   description: 'Веб-клиент для Sleepy - устройства для анализа стадий сна человека.',
+  mainNav: [
+    {
+      title: 'Главная',
+      href: '/',
+    },
+  ],
 };
